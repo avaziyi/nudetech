ec1ca1e00c1344f1f2dc0d88b6753f06175f1031
balaguer.israel@gmail.com <balaguer.israel@gmail.com@a4d49ea0-9e5d-e529-d120-bc8a629812f0>
2012-08-27 12:02:02 +0000

    Added logging capability using log4j Added copyright notes for codes taken from "Digital Image Processing An Algorithmic Approach using Java"

--- trunk/NudeTechWebApp/src/main/java/edu/amaes/nudetech/algo/regiondetect/labeling/SequentialLabeling.java
+++ trunk/NudeTechWebApp/src/main/java/edu/amaes/nudetech/algo/regiondetect/labeling/SequentialLabeling.java
@@ -1,3 +1,13 @@
+/**
+ * This sample code is made available as part of the book "Digital Image
+ * Processing - An Algorithmic Introduction using Java" by Wilhelm Burger
+ * and Mark J. Burge, Copyright (C) 2005-2008 Springer-Verlag Berlin, 
+ * Heidelberg, New York.
+ * Note that this code comes with absolutely no warranty of any kind.
+ * See http://www.imagingbook.com for details and licensing conditions.
+ * 
+ * Date: 2010/08/01
+ */
 package edu.amaes.nudetech.algo.regiondetect.labeling;
 
 import ij.IJ;
